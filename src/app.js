18af17e209234d4bab1648bba3eddf5e2587fd83
The-Ghost56 <131254680+The-Ghost56@users.noreply.github.com>
2024-11-25 14:22:23 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+function obtenerAleatorio(listas) {
+const indice = Math.floor(Math.random() * lista.length);
+return lista [indice]}
+
+function generarExcusa(){
+  let sujeto = obtenerAleatorio(who);
+  let accion = obtenerAleatorio(action);
+  let objeto = obtenerAleatorio(what);
+  let momento = obtenerAleatorio(when);
+
+  return `${sujeto} ${accion} ${objeto} ${momento}.`;
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  
+  document.querySelector("#excuse").innerHTML = generarExcusa
+};//write your code here
